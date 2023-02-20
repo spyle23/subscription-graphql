d84e7816efb9162e08ef3664d5f31413f0104d4b
spyle23 <sampingyen.lee@gmail.com>
2023-02-20 17:09:20 +0300

    add landing page and private route

---
+++ src/routers/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+import { CircularProgress } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import { useApplicationContext } from "../hooks";
+import { UserAuthStateEnum } from "../types/UserType";
+
+export const PrivateRoute = ({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element => {
+  const { userAuthStatus } = useApplicationContext();
+
+  if (userAuthStatus === UserAuthStateEnum.WAITING) {
+    return <CircularProgress />;
+  }
+  if (userAuthStatus === UserAuthStateEnum.AUTHENTICATED) {
+    return children;
+  }
+  return <Navigate to="/login" />;
+};
