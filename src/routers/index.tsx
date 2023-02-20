6099e28706fe24922409ffeb718142a77cb40e06
spyle23 <sampingyen.lee@gmail.com>
2023-02-20 16:27:46 +0300

    add guard and private route to application

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,5 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthenticationRoute } from "../pages/Authentication/AuthenticationRoute";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const MainRouter = (): JSX.Element => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route index element={<Navigate to="landing" />} />
+      <Route path="auth/*" element={<AuthenticationRoute />} />
+      <Route
+        path="landing/*"
+        element={
+          <PrivateRoute>
+            <PrivateRouter />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
 };
