201d094f607ac364f7e3a1fb1c1e4a41f0b612cf
spyle23 <sampingyen.lee@gmail.com>
2023-02-24 12:32:27 +0300

    update configuration apollo and security of app

---
+++ src/apollo.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  // service: {
+  //   endpoint: {
+  //     url: "http://localhost:4200/graphql",
+  //     headers: {
+  //       authorization: "Bearer " + import.meta.env.VITE_TOKEN_GENERATOR,
+  //     },
+  //   },
+  // },
+  client: {
+    service: {
+      url: "http://localhost:4200/graphql"
+    },
+  },
+};
