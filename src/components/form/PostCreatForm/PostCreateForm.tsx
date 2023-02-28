28b9c437c2dffae31207536bad17e2e961e5c606
spyle23 <sampingyen.lee@gmail.com>
2023-02-28 11:09:17 +0300

    add popup form for create post

--- src/components/form/PostCreatForm/PostCreateForm.tsx
+++ src/components/form/PostCreatForm/PostCreateForm.tsx
@@ -2,26 +2,44 @@ import {
   Box,
   Button,
   Card,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Grid,
   IconButton,
+  Slide,
   styled,
   TextField,
 } from "@mui/material";
 import { useApplicationContext } from "../../../hooks";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import CollectionsIcon from "@mui/icons-material/Collections";
-import { useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Dropzone } from "../../dropzone/Dropzone";
 import { useForm } from "react-hook-form";
 import { PostInput } from "../../../types/graphql-types";
 import { Delete } from "@mui/icons-material";
 import { usePost } from "../../../hooks/post/usePost";
 import { useFileDeleter } from "../../../hooks/application/useFileDeleter";
+import { TransitionProps } from "@mui/material/transitions";
+import { HeadCard } from "../headCard/HeadCard";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export const PostCreateForm = (): JSX.Element => {
   const { user } = useApplicationContext();
   const { createPost } = usePost();
   const [uploadPicture, setUploadPicture] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const { deleteFile } = useFileDeleter();
   const {
     register,
@@ -55,89 +73,135 @@ export const PostCreateForm = (): JSX.Element => {
     try {
       if (!user) return;
       await createPost(data, user.id);
+      handleClose();
     } catch (error) {
       console.log(error);
     }
   };
-  return (
-    <Card elevation={1} sx={{ width: { xs: "100%", md: 500 }, p: 2 }}>
-      <IconButton>
-        {user?.photo ? (
-          <img src={user.photo} alt="profile" />
-        ) : (
-          <FacebookOutlinedIcon />
-        )}
-      </IconButton>
-      <TextField
-        {...register("description", { required: true })}
-        error={errors.description && true}
-        sx={{
-          width: { md: 350, xs: 200 },
-        }}
-        InputProps={{
-          sx: {
-            borderRadius: "25px !important",
-          },
-        }}
-        placeholder="Exprimer vous"
-        helperText={
-          errors.image && "La description est requise pour une publication"
-        }
-      />
-      <IconButton onClick={() => setUploadPicture(true)}>
-        <CollectionsIcon />
-      </IconButton>
-      <Box sx={{ display: "flex", gap: 2 }}>
-        {uploadPicture && (
-          <Box
-            sx={{
-              width: 200,
-              height: 200,
-              py: 1,
-              position: "relative",
-            }}
-          >
-            <Dropzone
-              onFinished={handleChangePicture}
-              message="Ajouter une image"
-              btnSx={{ height: "100%" }}
-            />
-          </Box>
-        )}
-        {getValues().image && (
-          <Box
-            sx={{
-              width: 150,
-              height: 150,
-              mr: 1,
-              mb: 1,
-              position: "relative",
-            }}
-          >
-            <IconButton
-              onClick={deletePicture}
-              sx={{ position: "absolute", bottom: 5, right: 5 }}
-            >
-              <Delete color="error" />
-            </IconButton>
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-            <img
-              src={getValues().image || ""}
-              alt=""
-              width="100%"
-              height="100%"
-              style={{ objectFit: "cover" }}
-            />
-          </Box>
-        )}
-      </Box>
-      <Button
-        sx={{ my: 1 }}
-        variant="contained"
-        onClick={handleSubmit(handlePost)}
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Card elevation={1} sx={{ width: { xs: "100%", md: 500 }, p: 2 }}>
+        <IconButton>
+          {user?.photo ? (
+            <img src={user.photo} alt="profile" />
+          ) : (
+            <FacebookOutlinedIcon />
+          )}
+        </IconButton>
+        <TextField
+          sx={{
+            width: { md: 350, xs: 200 },
+          }}
+          InputProps={{
+            sx: {
+              borderRadius: "25px !important",
+            },
+          }}
+          onClick={handleClickOpen}
+          placeholder="Exprimer vous"
+        />
+        <IconButton
+          onClick={() => {
+            setUploadPicture(true);
+            handleClickOpen();
+          }}
+        >
+          <CollectionsIcon />
+        </IconButton>
+      </Card>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
       >
-        Publiez
-      </Button>
-    </Card>
+        <DialogTitle>
+          <HeadCard />
+        </DialogTitle>
+        <DialogContent
+          sx={{
+            pt: "20px !important",
+          }}
+        >
+          <TextField
+            {...register("description", { required: true })}
+            error={errors.description && true}
+            sx={{
+              width: { md: 350, xs: 200 },
+            }}
+            InputProps={{
+              sx: {
+                borderRadius: "25px !important",
+              },
+            }}
+            placeholder="Exprimer vous"
+            helperText={
+              errors.image && "La description est requise pour une publication"
+            }
+          />
+          {uploadPicture && (
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                py: 1,
+                my: 1,
+                position: "relative",
+              }}
+            >
+              <Dropzone
+                onFinished={handleChangePicture}
+                message="Ajouter une image"
+                btnSx={{ height: "100%" }}
+              />
+            </Box>
+          )}
+          <Box sx={{ display: "flex" }}>
+            {getValues().image && (
+              <Box
+                sx={{
+                  width: 150,
+                  height: 150,
+                  mr: 1,
+                  mb: 1,
+                  position: "relative",
+                }}
+              >
+                <IconButton
+                  onClick={deletePicture}
+                  sx={{ position: "absolute", bottom: 5, right: 5 }}
+                >
+                  <Delete color="error" />
+                </IconButton>
+
+                <img
+                  src={getValues().image || ""}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                  style={{ objectFit: "cover" }}
+                />
+              </Box>
+            )}
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="error" onClick={handleClose}>
+            Annuler
+          </Button>
+          <Button variant="contained" onClick={handleSubmit(handlePost)}>
+            Publier
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
---
+++ src/components/form/headCard/HeadCard.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import { useApplicationContext } from "../../../hooks";
+import DefaultProfil from "../../../assets/default_profil.png";
+import { AccountCircle } from "@mui/icons-material";
+
+export const HeadCard = (): JSX.Element => {
+  const { user } = useApplicationContext();
+
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ mr: 1 }} >
+        {user?.photo ? (
+          <img src={user?.photo} alt="profile" />
+        ) : (
+          <AccountCircle sx={{ fontSize: "2em" }} />
+        )}
+      </Box>
+      {user?.firstname && user?.lastname && (
+        <Typography>{user.firstname + " " + user.lastname}</Typography>
+      )}
+    </Box>
+  );
+};
