d504bd825bf3df8d5a35c2bce24e9be03538819e
spyle23 <sampingyen.lee@gmail.com>
2023-03-07 14:38:58 +0300

    add comment container

--- src/components/card/PostCard.tsx
+++ src/components/card/PostCard.tsx
@@ -9,40 +9,56 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { GetOrderPost_getOrderPost } from "../../graphql/post/types/GetOrderPost";
 import { HeadCard } from "../form/headCard/HeadCard";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import CommentIcon from "@mui/icons-material/Comment";
+import { CommentContainer } from "../comments/CommentContainer";
 
 type PostCardProps = {
   post: GetOrderPost_getOrderPost;
 } & CardProps;
 
 export const PostCard: FC<PostCardProps> = ({ post, ...cardProps }) => {
+  const [showComment, setShowComment] = useState<boolean>(false);
+  const handleToggleComment = () => {
+    setShowComment((curr) => !curr);
+  };
   return (
-    <Card elevation={1} {...cardProps}>
-      <CardHeader component={HeadCard}></CardHeader>
-      <CardContent>
-        <Typography>{post.description}</Typography>
-        {post.image && (
-          <img src={post.image} alt="post_image" style={{ width: "100%" }} />
-        )}
-      </CardContent>
-      <CardActions>
-        <Grid container>
-          <Grid item xs={6} sx={{ display: "flex", justifyContent: "center" }} >
-            <IconButton>
-              <ThumbUpIcon />
-            </IconButton>
+    <Box>
+      <Card elevation={1} {...cardProps}>
+        <CardHeader component={HeadCard}></CardHeader>
+        <CardContent>
+          <Typography>{post.description}</Typography>
+          {post.image && (
+            <img src={post.image} alt="post_image" style={{ width: "100%" }} />
+          )}
+        </CardContent>
+        <CardActions>
+          <Grid container>
+            <Grid
+              item
+              xs={6}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <IconButton>
+                <ThumbUpIcon />
+              </IconButton>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <IconButton onClick={handleToggleComment}>
+                <CommentIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item xs={6} sx={{ display: "flex", justifyContent: "center" }} >
-            <IconButton>
-              <CommentIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+      {showComment && <CommentContainer />}
+    </Box>
   );
 };
---
+++ src/components/comments/CommentContainer.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material"
+
+export const CommentContainer = ()=>{
+    return (
+        <Box>
+            <Typography>Comment Container works!</Typography>
+        </Box>
+    )
+}
