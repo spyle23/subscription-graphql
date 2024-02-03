5d4cb392adb0e4b2d5ba0a6b8c31104dd937308c
spyle23 <sampingyen.lee@gmail.com>
2024-02-03 15:06:03 +0300

    change ice servers for two peers

--- src/pages/VideoCall/index.tsx
+++ src/pages/VideoCall/index.tsx
@@ -145,22 +145,11 @@ const VideoCall = () => {
       stream,
       config: {
         iceServers: [
-          { urls: 'stun:stun.l.google.com:19302' },
-          {
-            urls: "turn:numb.viagenie.ca",
-            credential: "muazkh",
-            username: "webrtc@live.com",
-          },
-          {
-            urls: "turn:192.158.29.39:3478?transport=udp",
-            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-            username: "28224511:1379330808",
-          },
-          {
-            urls: "turn:192.158.29.39:3478?transport=tcp",
-            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-            username: "28224511:1379330808",
-          },
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun3.l.google.com:19302" },
+          { urls: "stun:stun4.l.google.com:19302" },
           {
             urls: "turn:turn.bistri.com:80",
             credential: "homeo",
@@ -201,22 +190,11 @@ const VideoCall = () => {
       stream,
       config: {
         iceServers: [
-          { urls: 'stun:stun.l.google.com:19302' },
-          {
-            urls: "turn:numb.viagenie.ca",
-            credential: "muazkh",
-            username: "webrtc@live.com",
-          },
-          {
-            urls: "turn:192.158.29.39:3478?transport=udp",
-            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-            username: "28224511:1379330808",
-          },
-          {
-            urls: "turn:192.158.29.39:3478?transport=tcp",
-            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-            username: "28224511:1379330808",
-          },
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun3.l.google.com:19302" },
+          { urls: "stun:stun4.l.google.com:19302" },
           {
             urls: "turn:turn.bistri.com:80",
             credential: "homeo",
